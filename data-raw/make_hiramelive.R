d3165fc938a932b48a3398faa74305361d9d00c5
Akihiro Manabe <amanabe@affrc.go.jp>
2021-01-27 16:36:23 +0900

    :tada: Add make_hiramelive

---
+++ data-raw/make_hiramelive.R
@@ -0,0 +1,135 @@
+devtools::load_all()
+
+# Hirame live data -----
+## 2002 ----
+
+hiramelive_2002 <-
+  read_hiramelive(fname = "./data-raw/excel/2002_hiramelive.csv")
+
+usethis::use_data(hiramelive_2002, overwrite = TRUE)
+
+## 2003 ----
+
+hiramelive_2003 <-
+  read_hiramelive(fname = "./data-raw/excel/2003_hiramelive.csv")
+
+usethis::use_data(hiramelive_2003, overwrite = TRUE)
+
+## 2004 ----
+
+hiramelive_2004 <-
+  read_hiramelive(fname = "./data-raw/excel/2004_hiramelive.csv")
+
+usethis::use_data(hiramelive_2004, overwrite = TRUE)
+
+## 2005 ----
+
+hiramelive_2005 <-
+  read_hiramelive(fname = "./data-raw/excel/2005_hiramelive.csv")
+
+usethis::use_data(hiramelive_2005, overwrite = TRUE)
+
+## 2006 ----
+
+hiramelive_2006 <-
+  read_hiramelive(fname = "./data-raw/excel/2006_hiramelive.csv")
+
+usethis::use_data(hiramelive_2006, overwrite = TRUE)
+
+## 2007 ----
+
+hiramelive_2007 <-
+  read_hiramelive(fname = "./data-raw/excel/2007_hiramelive.csv")
+
+usethis::use_data(hiramelive_2007, overwrite = TRUE)
+
+## 2008 ----
+
+hiramelive_2008 <-
+  read_hiramelive(fname = "./data-raw/excel/2008_hiramelive.csv")
+
+usethis::use_data(hiramelive_2008, overwrite = TRUE)
+
+## 2009 ----
+
+hiramelive_2009 <-
+  read_hiramelive(fname = "./data-raw/excel/2009_hiramelive.csv")
+
+usethis::use_data(hiramelive_2009, overwrite = TRUE)
+
+## 2010 ----
+
+hiramelive_2010 <-
+  read_hiramelive(fname = "./data-raw/excel/2010_hiramelive.csv")
+
+usethis::use_data(hiramelive_2010, overwrite = TRUE)
+
+## 2011 ----
+
+hiramelive_2011 <-
+  read_hiramelive(fname = "./data-raw/excel/2011_hiramelive.csv")
+
+usethis::use_data(hiramelive_2011, overwrite = TRUE)
+
+## 2012 ----
+
+hiramelive_2012 <-
+  read_hiramelive(fname = "./data-raw/excel/2012_hiramelive.csv")
+
+usethis::use_data(hiramelive_2012, overwrite = TRUE)
+
+## 2013 ----
+
+hiramelive_2013 <-
+  read_hiramelive(fname = "./data-raw/excel/2013_hiramelive.csv")
+
+usethis::use_data(hiramelive_2013, overwrite = TRUE)
+
+## 2014 ----
+
+hiramelive_2014 <-
+  read_hiramelive(fname = "./data-raw/excel/2014_hiramelive.csv")
+
+usethis::use_data(hiramelive_2014, overwrite = TRUE)
+
+## 2015 ----
+
+hiramelive_2015 <-
+  read_hiramelive(fname = "./data-raw/excel/2015_hiramelive.csv")
+
+usethis::use_data(hiramelive_2015, overwrite = TRUE)
+
+## 2016 ----
+
+hiramelive_2016 <-
+  read_hiramelive(fname = "./data-raw/excel/2016_hiramelive.csv")
+
+usethis::use_data(hiramelive_2016, overwrite = TRUE)
+
+## 2017 ----
+
+hiramelive_2017 <-
+  read_hiramelive(fname = "./data-raw/excel/2017_hiramelive.csv")
+
+usethis::use_data(hiramelive_2017, overwrite = TRUE)
+
+## 2018 ----
+
+hiramelive_2018 <-
+  read_hiramelive(fname = "./data-raw/excel/2018_hiramelive.csv")
+
+usethis::use_data(hiramelive_2018, overwrite = TRUE)
+
+## 2019 ----
+
+hiramelive_2019 <-
+  read_hiramelive(fname = "./data-raw/excel/2019_hiramelive.csv")
+
+usethis::use_data(hiramelive_2019, overwrite = TRUE)
+
+## 2020 ----
+
+hiramelive_2020 <-
+  read_hiramelive(fname = "./data-raw/excel/2020_hiramelive.csv")
+
+usethis::use_data(hiramelive_2020, overwrite = TRUE)
